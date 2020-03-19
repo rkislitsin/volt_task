efd26c29e5a1b58d02a815ac118bf9a135ba0003
arebeb <romakislitsin89@gmail.com>
2020-03-19 22:34:43 +0600

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  command = Command.create(name: Faker::Sports::Football.team)
+  11.times { Player.create(name: Faker::Sports::Football.player, command: command) }
+end
+
+5.times do |i|
+  home_command = Command.all[i]
+  Command.where.not(id: home_command.id).each do |guest_command|
+    Match.create(name: Faker::Company.name, home_command: home_command, guest_command: guest_command)
+  end
+end
+
+10.times { Achievement.create(name: Faker::Superhero.power) }
+
+500.times { Rating.create(player: Player.all.sample, achievement: Achievement.all.sample, match: Match.all.sample) }
