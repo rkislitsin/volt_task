9e16427e903b41e34b31977ce0d4e6170b8cf903
arebeb <romakislitsin89@gmail.com>
2020-03-19 22:09:09 +0600

    add last_matches scope to Match

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,6 +2,8 @@ class Match < ApplicationRecord
   belongs_to :home_command, class_name: 'Command', foreign_key: 'home_command_id'
   belongs_to :guest_command, class_name: 'Command', foreign_key: 'guest_command_id'
 
+  scope :last_5_matches, ->(command) { where(home_command: command).or(where(guest_command: command)).limit(5) }
+
   validates :home_command_id, uniqueness: { scope: :guest_command_id }
   validates_presence_of :name
   validates_presence_of :home_command
